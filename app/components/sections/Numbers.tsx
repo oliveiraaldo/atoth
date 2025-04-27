6d8d0d6f98fb84bb9885817c2ff7fe70279f5d3a
06682865635 <06682865635@correios.com.br>
2025-04-27 11:34:58 -0300

    Refatora o componente Numbers para melhorar a apresentação dos dados, substituindo a estrutura de layout por uma abordagem mais simples e responsiva. Ajusta os estilos dos ícones e textos para uma melhor legibilidade e estética.

--- app/components/sections/Numbers.tsx
+++ app/components/sections/Numbers.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { TbReportAnalytics } from "react-icons/tb";
 import { PiUsersThree } from "react-icons/pi";
 import { LuCalendarCheck2 } from "react-icons/lu";
@@ -6,83 +5,57 @@ import { BsBuildingCheck } from "react-icons/bs";
 
 export default function Numbers() {
   return (
-    <section className="numbers bg-blue-50 py-15 container mx-auto px-10 md:px-0">
+    <section className="numbers bg-blue-50 p-8">
       <div className="flex flex-col items-center justify-center">
-        <h3 className="text-blue-500 text-[36px] text-center">
+        <h3 className="text-blue-500 text-[36px]">
           Números que comprovam nossa excelência
         </h3>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 py-12 container mx-auto">
-
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 pt-12 container mx-auto">
         {/* Auditorias */}
-        <div className="flex items-center justify-center gap-4">
-          <div className="icon text-blue-500 text-[60px]">  
-            <TbReportAnalytics />
+        <div className="flex flex-col items-center">
+          <div className="text-blue-500 text-5xl mb-4">
+            <TbReportAnalytics className="h-16 w-16" />
           </div>
-          <div className="right-number text-center">
-            <div className="number text-blue-900 text-[28px] md:text-[32px] font-bold">
-              300 <span className="text-blue-500">+</span>
-            </div>
-            <div className="description">
-              <p className="text-blue-900 text-center font-medium">
-                Auditorias<br />Realizadas
-              </p>
-            </div>
-          </div>
+          <h2 className="text-blue-900 text-5xl font-bold mb-2">300<span className="text-blue-500">+</span></h2>
+          <p className="text-blue-900 text-center font-medium">
+            Auditorias<br />Realizadas
+          </p>
         </div>
 
-        {/* Treinamentos */}
-        <div className="flex items-center justify-center gap-4">
-          <div className="icon text-blue-500 text-[60px]">  
-            <PiUsersThree />
+        {/* Profissionais */}
+        <div className="flex flex-col items-center">
+          <div className="text-blue-500 text-5xl mb-4">
+            <PiUsersThree className="h-16 w-16" />
           </div>
-          <div className="right-number text-center">
-            <div className="number text-blue-900 text-[28px] md:text-[32px] font-bold">
-              500 <span className="text-blue-500">+</span>
-            </div>
-            <div className="description">
-              <p className="text-blue-900 text-center font-medium">
-                Profissionais<br />Treinados
-              </p>
-            </div>
-          </div>
+          <h2 className="text-blue-900 text-5xl font-bold mb-2">500<span className="text-blue-500">+</span></h2>
+          <p className="text-blue-900 text-center font-medium">
+            Profissionais<br />Treinados
+          </p>
         </div>
 
         {/* Sistemas */}
-        <div className="flex items-center justify-center gap-4">
-          <div className="icon text-blue-500 text-[60px]">  
-            <BsBuildingCheck />
+        <div className="flex flex-col items-center">
+          <div className="text-blue-500 text-5xl mb-4">
+            <BsBuildingCheck className="h-16 w-16" />
           </div>
-          <div className="right-number text-center">
-            <div className="number text-blue-900 text-[28px] md:text-[32px] font-bold">
-              600 <span className="text-blue-500">+</span>
-            </div>
-            <div className="description">
-              <p className="text-blue-900 text-center font-medium">
-                Sistemas<br />Implementados
-              </p>
-            </div>
-          </div>
+          <h2 className="text-blue-900 text-5xl font-bold mb-2">600<span className="text-blue-500">+</span></h2>
+          <p className="text-blue-900 text-center font-medium">
+            Sistemas<br />Implementados
+          </p>
         </div>
 
-        {/* Prazo */}
-        <div className="flex items-center justify-center gap-4">
-          <div className="icon text-blue-500 text-[60px]">  
-            <LuCalendarCheck2 />
+        {/* Entregas */}
+        <div className="flex flex-col items-center">
+          <div className="text-blue-500 text-5xl mb-4">
+            <LuCalendarCheck2 className="h-16 w-16" />
           </div>
-          <div className="right-number text-center">
-            <div className="number text-blue-900 text-[28px] md:text-[32px] font-bold">
-              100 <span className="text-blue-500">%</span>
-            </div>
-            <div className="description">
-              <p className="text-blue-900 text-center font-medium">
-                Entregues<br />no Prazo
-              </p>
-            </div>
-          </div>
+          <h2 className="text-blue-900 text-5xl font-bold mb-2">100<span className="text-blue-500">%</span></h2>
+          <p className="text-blue-900 text-center font-medium">
+            Entregas no<br />prazo
+          </p>
         </div>
-
       </div>
     </section>
   )
